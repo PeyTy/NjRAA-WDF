68ded01057474550b5d1ac9e333b72a4d6f04710
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2021-09-08 19:12:29 +0300

    Add header and license

--- kmodule/njraa.c
+++ kmodule/njraa.c
@@ -1,6 +1,23 @@
+// NjRAA
+// Copyright (C) 2021  Oleg Petrenko
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/pci.h>
+
+#include "njraa.h"
 
 #define NJ_DRIVER "njraa"
 
